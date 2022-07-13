7b8874748076d397731a585fff41b9aebfeea9be
pathfindertogami <pathfindertogami@mail.ru>
2022-07-13 16:37:31 +0500

    Update eventscript.js

--- eventscript.js
+++ eventscript.js
@@ -7,5 +7,6 @@ function isChecked(){
   else{
     document.getElementById("message1").textContent = "Перед тем как продолжить, прочитайте правила и описание ивента https://docs.google.com/document/d/1uPLzFK7pqIJu7NUlydC04pEnTATez5xlCB1SZofdEEk/edit?usp=drivesdk"
     document.getElementById("message2").textContent = " ";
+    document.getElementById("text").textContent = "из за ебучего джаваскрипта придется копировать ссылку и вставлять ее вручную";
   }
 }
