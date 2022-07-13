b625ea90af34a1ca1c6c59ac57d52d24119c12f1
pathfindertogami <pathfindertogami@mail.ru>
2022-07-13 16:36:50 +0500

    r

--- eventscript.js
+++ eventscript.js
@@ -1,8 +1,11 @@
 function isChecked(){
   if(document.getElementById("podtv").checked){
-    document.getElementById("message").textContent = "Теперь вы можете перейти дальше https://pathfindertogami.github.io/eventvybor.html";
+    document.getElementById("message2").textContent = "https://pathfindertogami.github.io/eventvybor.html";
+    document.getElementById("message1").textContent = " ";
+    document.getElementById("text").textContent = "из за ебучего джаваскрипта придется копировать ссылку и вставлять ее вручную";
   }
   else{
-    document.getElementById("message").textContent = "Перед тем как продолжить, прочитайте правила и описание ивента https://docs.google.com/document/d/1uPLzFK7pqIJu7NUlydC04pEnTATez5xlCB1SZofdEEk/edit?usp=drivesdk"
+    document.getElementById("message1").textContent = "Перед тем как продолжить, прочитайте правила и описание ивента https://docs.google.com/document/d/1uPLzFK7pqIJu7NUlydC04pEnTATez5xlCB1SZofdEEk/edit?usp=drivesdk"
+    document.getElementById("message2").textContent = " ";
   }
 }
