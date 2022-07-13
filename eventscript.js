6d6fa214080a9e26fe9fe16574713e12cee69706
pathfindertogami <pathfindertogami@mail.ru>
2022-07-13 15:55:34 +0500

    й

--- eventscript.js
+++ eventscript.js
@@ -1,6 +1,6 @@
 function isChecked(){
   if(document.getElementById("podtv").checked){
-    document.getElementById("message").textContent = "Теперь вы можете перейти дальше https://pathfindertogami.github.io/vybor.html";
+    document.getElementById("message").textContent = "Теперь вы можете перейти дальше https://pathfindertogami.github.io/eventvybor.html";
   }
   else{
     document.getElementById("message").textContent = "Перед тем как продолжить, прочитайте правила и описание ивента https://docs.google.com/document/d/1uPLzFK7pqIJu7NUlydC04pEnTATez5xlCB1SZofdEEk/edit?usp=drivesdk"
