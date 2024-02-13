850afa6c556cfd1a957b98bbb8520cd64e109f82
pathfindertogami <pathfindertogami@mail.ru>
2024-02-14 01:19:38 +0500

    ремейк старой хуйни

--- script.js
+++ script.js
@@ -1,63 +1,51 @@
-/*function browserDetect() {
+const apikey = "1f16619820946295c600c1c3d7013207"
+const url = `https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=rj&api_key=${apikey}&format=json&user=pathfinder2gami`
+
+function browserDetect() {
     let userAgent = navigator.userAgent;
     let browserName;
     if ((navigator.userAgent.indexOf('OPR')) != -1 ) {
-        browserName = "оперу";
+        browserName = "opera";
     } else if (navigator.userAgent.indexOf("Edg") != -1 ) { 
-        browserName = "эдж"; 
+        browserName = "edge"; 
     } else if (navigator.userAgent.indexOf("Chrome") != -1 ) { 
-        browserName = "хром"; 
+        browserName = "chrome"; 
     } else if (navigator.userAgent.indexOf("Firefox") != -1 ) { 
         browserName = "firefox"; 
     };
-    document.getElementById("chrome").innerHTML="снеси" + " " + browserName + " ";
+    document.getElementById("browser").textContent = browserName;
     if (browserName != "firefox") {
-        document.getElementById("chrome").style.display = `flex`;
-        document.getElementById("main").style.display = `none`;
-        document.getElementById("firefoxdl").style.display = `flex`;
-        document.getElementById("notfullscreen").style.display = `none`;
+        document.getElementById("chromium").style.visibility = `visible`;
     }
 }
-function randomSong() {
-    src = ["https://audio.jukehost.co.uk/jAhLqGYIvSW9qwgaptyLDhIKVcj2jfB0", "https://audio.jukehost.co.uk/kIV5aMsrUHcgYuqOoq5z3JnoYz88EFt4", "https://audio.jukehost.co.uk/MhoEruseoO7aNFh1CWXOJMoFBHB4gzex", "https://audio.jukehost.co.uk/A78R2kQyWVJqD6osXPEopDqGP302zyNo", "https://audio.jukehost.co.uk/cWpDr4JHa0ZgUh18Rv7xGZUKkRLP4Gix", "https://audio.jukehost.co.uk/AGCggHySS3Dkk5j0cHsttxb7JbHQyGA5",  "https://audio.jukehost.co.uk/3pGyQPezHLVYTMsrPIlfxaazEBmfABtI", "https://audio.jukehost.co.uk/XxaQGl2XoK95eDKSJZKdAKJ535hTqZXH", "https://audio.jukehost.co.uk/Olx6arQCYgjuD3QIQgP5inY2sSg1Ohsc", "https://audio.jukehost.co.uk/QGOmzwJxYJ7ZvwbhyQvq1NHRFiJS7nme", "https://audio.jukehost.co.uk/2VR8bSLZoiYm4TC81sBCycQH9d8iJOD0", "https://audio.jukehost.co.uk/fBL7XcbWYSUqba4uht7RDXanZvHXqaWx"]
-    random = Math.floor(Math.random() * (Math.floor(11) - Math.ceil(0)) + Math.ceil(0))
-    document.getElementById("audio").src = src[random]
+function GET(url) 
+{
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", url, false);
+    xhr.send(null);
+    return xhr.responseText;
 }
-function windowSize() {
-    var width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-    var height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
-    document.getElementById("notfullscreen").innerHTML ="(>ω< ) <br> никаких экранов/окон размером меньше 1450x850 <br><br>" + width + "/1450" + " " + height + "/850"
+function gettrack(url)
+{
+    var json = JSON.parse(GET(url));
+    var lasttrack = json.recenttracks.track[0]
+    var track = lasttrack.name
+    var tracklink = lasttrack.url
+    var artist = lasttrack.artist['#text']
+    //var cover = lasttrack.image[1]['#text']
+    document.getElementById("track").textContent = artist + " - " + track;
+    document.getElementById("track").href = tracklink;
+    setTimeout(gettrack, 1000);
 }
-function soundonhover() {
-    document.getElementById("hover").play();
-    document.getElementById("revoh").currentTime = 0;
+function uptime() {
+    var start = new Date("Apr 4, 2008 09:00:00 GMT+0500");
+    var now = new Date();
+    let uptime = Math.abs(now.getTime() - start.getTime())/1000;
+    let days = Math.round(uptime / 86400);
+    let hours = Math.round((days * 24) - uptime / 3600);
+    document.getElementById("uptime").textContent = days + " days, " + hours + " hours";
+    setTimeout(uptime, 10);
 }
-function resetonhover() {
-    document.getElementById("hover").pause();
-    document.getElementById("hover").currentTime = 0;
-    document.getElementById("revoh").play();
-}
-function togglePlay() {
-    isPlaying ? document.getElementById("audio").pause() : document.getElementById("audio").play();
-};
-document.getElementById("audio").onplaying = function() {
-    isPlaying = true;
-    document.getElementById("muteicon").src = "https://gachi.gay/VWJ6y";
-};
-document.getElementById("audio").onpause = function() {
-    isPlaying = false;
-    document.getElementById("muteicon").src = "https://gachi.gay/M0HS8";
-};
-document.querySelectorAll("img").forEach(function (img) {
-    img.addEventListener('mouseover', soundonhover);
-});
-document.querySelectorAll("img").forEach(function (img) {
-    img.addEventListener('mouseout', resetonhover);
-});*/
-document.getElementById("main").style.display = `none`;
-document.getElementById("notfullscreen").style.display = `none`;
-document.getElementById("muteicon").style.display = `none`;
-//window.onload = randomSong();
-//window.onload = browserDetect();
-//window.onload = windowSize();
-//window.onresize = windowSize;
+gettrack(url);
+browserDetect();
+uptime();
